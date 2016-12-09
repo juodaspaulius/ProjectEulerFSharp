c898ff4a727510188ffa29461f7307c9ccb382ca
Paulius Juodisius <juodaspaulius@gmail.com>
2016-12-08 21:39:40 -0500

    Problem 001

---
+++ ProjectEuler/Problem001.fs
@@ -0,0 +1,12 @@
+﻿module Problem001
+
+let rec integersFrom n fn = seq { 
+  yield n
+  yield! integersFrom (fn n) fn }
+
+let answer limit = 
+    let limitSeq = Seq.takeWhile (fun n -> n < limit)
+    let seq3 = integersFrom 0 ((+) 3) |> limitSeq 
+    let seq5 = integersFrom 0 ((+) 5) |> limitSeq 
+    let seq15 = integersFrom 0 ((+) 15) |> limitSeq 
+    (seq3 |> Seq.sum) + (seq5 |> Seq.sum) - (seq15 |> Seq.sum)
---
+++ ProjectEuler/Program.fs
@@ -0,0 +1,10 @@
+﻿open System
+open System.IO
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+
+    printfn "Problem1: %A" (Problem001.answer 1000)
+    Console.ReadLine()
+    0
